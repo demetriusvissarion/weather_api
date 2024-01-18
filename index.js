87546f546c3bdd50848f37b4e2a3112c3f8a48be
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-01-18 17:18:58 +0000

    check specific fields

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${
 let weatherData = null;
 
 fetch(apiUrl).then((response) => response.json()).then((weatherData) => {
-    console.log(weatherData)
+    // console.log(weatherData)
+    console.log(weatherData.main.temp);
+    console.log(weatherData.weather[0].main);
 });
 
 console.log('Requesting weather data');
